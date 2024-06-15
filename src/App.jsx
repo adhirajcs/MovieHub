e70d0294cdf3709880e5b268e9d7f489ce65408a
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-06-15 15:28:26 +0000

    Added the Styles and API Key

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+
+const API_URL = `http://www.omdbapi.com?apikey=${
+  import.meta.env.VITE_OMDB_API_KEY
+}`;
+
+const App = () => {
+  const searchMovies = async (title) => {
+    try {
+      const response = await axios.get(`${API_URL}&s=${title}`);
+      const data = response.data;
+
+      console.log(data.Search);
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    searchMovies("Superman");
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Hello</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
