a06231f094088d3a181961387bfdde34263aed99
Adhiraj Saha <93763060+adhirajcs@users.noreply.github.com>
2024-06-16 15:23:39 +0000

    Added the search functionality for all movies and successfully fetched data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import MovieCard from './MovieCard'
+import MovieCard from "./MovieCard";
 
 import "./App.css";
 import searchIcon from "./search.svg";
@@ -20,19 +20,22 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     try {
       const response = await axios.get(`${API_URL}&s=${title}`);
       const data = response.data;
 
-      console.log(data.Search);
+      setMovies(data.Search);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
   useEffect(() => {
-    searchMovies("Superman");
+    searchMovies(searchTerm);
   }, []);
 
   return (
@@ -43,15 +46,22 @@ const App = () => {
         <div className="search">
           <input
             placeholder="Search For Movies"
-            value="Superman"
-            onChange={() => {}}
+            onChange={(e) => {setSearchTerm(e.target.value)}}
           />
-          <img src={searchIcon} alt="Search" onClick={() => {}} />
-
-          <div className="container">
-            <MovieCard movie1={movie1} />
-          </div>
+          <img src={searchIcon} alt="Search" onClick={() => {searchMovies(searchTerm)}} />
         </div>
+
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No Movies found</h2>
+          </div>
+        )}
       </div>
     </>
   );
